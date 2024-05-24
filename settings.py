7fd6fe0a9321fedadbd46eb2691b92fbc4a0f18b
olgarpsb <olgarpsb@gmail.comgit config --global user.email olgarpsb@gmail.com>
2024-05-24 00:01:58 -0400

    1

---
+++ settings.py
@@ -0,0 +1,17 @@
+# PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'olgargroup',
+        'PASSWORD': 'MjE0OC1vbGdhcmdy',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'crud',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
